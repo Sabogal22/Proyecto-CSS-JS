0bb030d949f9d826b9efa59c641eb57d1e371340
Sabogal22 <nsabogalgaitan@gmail.com>
2023-02-24 16:09:01 -0500

    Random Emoji Generator

---
+++ generador-de-emojis/script.js
@@ -0,0 +1,8 @@
+const emoji = ["😮", "😲", "😴", "🤤", "😪", "😵", "🤐", "🥴", "🤢", "🤮", "🤕", "🥳", "😷", "🤒", "🤕", "🤑", "🤠", "😈", "👿", "👹", "👺", "😿", "😾", "😀", "😃", "😄", "😁", "😆", "😅", "🤣", "😂", "🙂", "💀", "👻", "👽", "🤖", "💩", "😺", "😸", "😹", "😻", "😼", "🤭", "🤫", "🤥", "😶", "😐", "😑", "😬", "🙄", "😯", "😦", "😧"]
+
+const generateBtn = document.getElementById('btn');
+document.getElementById("emoji-container").textContent = emoji[0];
+generateBtn.addEventListener('click', function () {
+  let output = emoji[Math.floor(Math.random() * emoji.length)];
+  document.getElementById("emoji-container").textContent = output;
+})
