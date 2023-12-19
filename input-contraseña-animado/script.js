1a54241a46f934d6c4c836feb845f1d8671d4b3b
Sabogal22 <nsabogalgaitan@gmail.com>
2023-12-19 14:53:41 -0500

    Input Field Animation

---
+++ input-contraseña-animado/script.js
@@ -0,0 +1,8 @@
+const inputIcon = document.querySelector(".input_icon");
+const inputPassword = document.querySelector(".input_field");
+
+inputIcon.addEventListener("click", () => {
+  inputIcon.classList.toggle("ri-eye-off-line");
+  inputIcon.classList.toggle("ri-eye-line");
+  inputPassword.type = inputPassword.type === "password" ? "text" : "password";
+});
