250865ddb63f188f6e711df7ac98dc27373a8da2
Sabogal22 <nsabogalgaitan@gmail.com>
2023-12-19 15:37:29 -0500

    Calculator

--- calculadora/script.js
+++ calculadora/script.js
@@ -3,27 +3,27 @@ const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
-//Define function to calculate based on button clicked.
+// Defina la función para calcular según el botón hecho clic.
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    //If output has '%', replace with '/100' before evaluating.
+    // Si la salida tiene '%', reemplácela con '/100' antes de evaluar.
     output = eval(output.replace("%", "/100"));
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
-    //If DEL button is clicked, remove the last character from the output.
+    // Si se hace clic en el botón DEL, elimine el último carácter de la salida.
     output = output.toString().slice(0, -1);
   } else {
-    //If output is empty and button is specialChars then return
+    // Si la salida está vacía y el botón tiene caracteres especiales, regrese
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
   display.value = output;
 };
 
-//Add event listener to buttons, call calculate() on click.
+// Agregue un detector de eventos a los botones, llame a calcular() al hacer clic.
 buttons.forEach((button) => {
-  //Button click listener calls calculate() with dataset value as argument.
+  // El oyente que hace clic en el botón llama a calcular() con el valor del conjunto de datos como argumento.
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
